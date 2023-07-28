a473ede3ed82416bc56d0196fbd53a890a1a8ef8
Mohammed Mostafa <128194288+mohammedd20@users.noreply.github.com>
2023-07-28 19:29:00 +0300

    Delete E-Commerce-nodeJs-main directory

--- E-Commerce-nodeJs-main/CustomError.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ stateCod = 500, code, message }) => {
-  const CustomError = new Error(message);
-  (CustomError.code = code), (CustomError.status = stateCod);
-  return CustomError;
-};
